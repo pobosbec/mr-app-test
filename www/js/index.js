b8805f3f8c102364d1762664e58f971dce4acd92
P-O <p-o@bosbec.se>
2016-01-20 16:10:06 +0100

    Update config and creating db

--- www/js/index.js
+++ www/js/index.js
@@ -64,5 +64,16 @@ var app = {
         receivedElement.setAttribute('style', 'display:block;');
 
         console.log('Received Event: ' + id);
+
+        var db = window.sqlitePlugin.openDatabase({name: "my.db", location: 1}, successcb, errorcb);
+        myDB.transaction(function(transaction) {
+          transaction.executeSql('CREATE TABLE IF NOT EXISTS bosbec (id integer primary key, title text, desc text)', [],
+          function(tx, result) {
+            alert("Table created successfully");
+          },
+          function(error) {
+            alert("Error occurred while creating the table.");
+          });
+        });
     }
 };
