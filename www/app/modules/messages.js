cd09c9a4cd600809503502f8809a2f27a668a273
Robin Pipirs <robinpipirs@gmail.com>
2016-03-15 15:53:36 +0100

    added protractor and one fake test
    added protractor and one fake test

--- www/app/modules/messages.js
+++ www/app/modules/messages.js
@@ -14,4 +14,5 @@ angular.module('messages', [])
             $scope.messages = messageRepository.getMessages();
             //$scope.$apply();
         });
+
     }]);
---
+++ www/tests/conf.js
@@ -0,0 +1,7 @@
+/**
+ * Created by Robin Jobb on 2016-03-15.
+ */
+exports.config = {
+    seleniumAddress: 'http://localhost:4444/wd/hub',
+    specs: ['messages-spec.js']
+};
