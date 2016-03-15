09c2f1112415ae15a72a3be977d74bb9f31b30e5
Robin Pipirs <robinpipirs@gmail.com>
2016-03-15 13:41:43 +0100

    removed test function
    removed test function

--- www/app/modules/messages.js
+++ www/app/modules/messages.js
@@ -10,19 +10,6 @@ angular.module('messages', [])
             $rootScope.$broadcast('download-whats-new');
         }, 10000);
 
-        /**
-         * Just for testing
-         */
-        //$scope.$on('new-messages', function (event,args ) {
-        //    console.log("new-message");
-        //    if(args.length > 0){
-        //        $scope.messages = [];
-        //        for(var msg in args){
-        //            $scope.messages.push(args[msg]);
-        //        }
-        //    }
-        //});
-
         $scope.$on('messages-added', function (event, args) {
             $scope.messages = messageRepository.getMessages();
             //$scope.$apply();
