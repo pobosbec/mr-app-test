adcd15fe120248305f505d54833724ba1375b0d5
Robin Pipirs <robinpipirs@gmail.com>
2016-03-15 11:39:58 +0100

    added get
    get messages

--- www/app/modules/messages.js
+++ www/app/modules/messages.js
@@ -3,18 +3,25 @@
  */
 angular.module('messages', [])
     .controller('messagesController', ['$scope', '$http', '$rootScope', 'messageRepository', 'communicationService', function ($scope, $http, $rootScope, messageRepository, communicationService) {
-        $scope.messages = [];
+        $scope.messages = messageRepository.getMessages();
 
         setInterval(function(){
             $rootScope.$broadcast('download-whats-new');
         }, 10000);
 
 
+        /**
+         * Just for testing
+         */
         $scope.$on('new-messages', function (event,args ) {
             console.log("new-message");
-            for(var msg in args){
-                $scope.messages.push(args[msg]);
+            if(args.length > 0){
+                $scope.messages = [];
+                for(var msg in args){
+                    $scope.messages.push(args[msg]);
+                }
             }
+
 
         });
 
