f26ef05bac791464f2627c233d7c18bf91181845
Kristofer Holmgren <kristofer@bosbec.se>
2016-03-16 13:44:35 +0100

    Fixed name for pushwoos plugin.

--- www/js/PushwooshWP.js
+++ www/js/PushwooshWP.js
@@ -18,7 +18,7 @@
  */
 
 function registerPushwooshWP() {
-    var pushNotification = cordova.require("com.pushwoosh.plugins.pushwoosh.PushNotification");
+    var pushNotification = cordova.require("pushwoosh-cordova-plugin.PushNotification");
 
     //set push notification callback before we initialize the plugin
     document.addEventListener('push-notification', function (event) {
@@ -49,7 +49,7 @@ function registerPushwooshWP() {
 
 function onPushwooshWPInitialized()
 {
-    var pushNotification = cordova.require("com.pushwoosh.plugins.pushwoosh.PushNotification");
+    var pushNotification = cordova.require("pushwoosh-cordova-plugin.PushNotification");
 
     //if you need push token at a later time you can always get it from Pushwoosh plugin
     pushNotification.getPushToken(
--- www/js/index.js
+++ www/js/index.js
@@ -17,7 +17,7 @@
  * under the License.
  */
 function initPushwoosh() {
-    var pushNotification = cordova.require("com.pushwoosh.plugins.pushwoosh.PushNotification");
+    var pushNotification = cordova.require("pushwoosh-cordova-plugin.PushNotification");
     if (device.platform == "Android") {
         registerPushwooshAndroid();
     }
