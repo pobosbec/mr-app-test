61764025623c4743f60ac0aab409d910f9f80d37
Magnus Svensson <magnus@bosbec.se>
2016-03-17 15:33:57 +0100

    fetching ids and data in order

--- www/app/factories/token-factory.js
+++ www/app/factories/token-factory.js
@@ -112,7 +112,7 @@ angular.module('token', [])
                 adminId = response.data.data.administratorId;
                 accountId = response.data.data.accountId;
             }).then(factory.isAppAuthenticated(factory.getAppAuthToken())).then(function (response) {
-                
+
             });
             return response;
         };
@@ -167,13 +167,13 @@ angular.module('token', [])
 
             return $http(req
             ).then(function successCallback(response) {
-                    // this callback will be called asynchronously
-                    // when the response is available
-                    //things to fetch
-                    token = response.data.data.id;
-                    $rootScope.token = token;
-                    adminId = response.data.data.administratorId;
-                    accountId = response.data.data.accountId;
+                // this callback will be called asynchronously
+                // when the response is available
+                //things to fetch
+                token = response.data.data.id;
+                $rootScope.token = token;
+                adminId = response.data.data.administratorId;
+                accountId = response.data.data.accountId;
                 // this callback will be called asynchronously
                 // when the response is available
 
@@ -347,8 +347,18 @@ angular.module('token', [])
 
         factory.getAuthToken = function () {
             if (token == null) {
-                token = JSON.parse(localStorage.getItem("authToken"));
+                if (win.sessionStorage.accessToken != null) {
+                    token = win.sessionStorage.accessToken;
+                } else if (localStorage.getItem("authToken") != null) {
+                    token = JSON.parse(localStorage.getItem("authToken"));
+                } else {
+                    var refreshIds = factory.refreshIds();
+                    refreshIds.then(function (response) {
+                        return token;
+                    });
+                }
             }
+
             return token;
         };
 
@@ -358,16 +368,19 @@ angular.module('token', [])
             }
             return appToken;
         };
-        factory.getAppUserId = function () {
 
-            if (appUserId === null) {
-                var refreshIds = factory.refreshIds();
-                refreshIds.then(function (response) {
-                    return appUserId;
-                });
-            } else {
-                return appUserId;
+        factory.getAppUserId = function () {
+            if (appUserId == null) {
+                if (localStorage.getItem("appUserId") != null) {
+                    appUserId = JSON.parse(localStorage.getItem("appUserId"));
+                } else {
+                    var refreshIds = factory.refreshIds();
+                    refreshIds.then(function (response) {
+                        return appUserId;
+                    });
+                }
             }
+
             return appUserId;
         };
 
