cc82d94b65d98450c10c5dc84a33d5465a9eb00f
Robin Pipirs <robinpipirs@gmail.com>
2016-03-22 12:19:53 +0100

    card on click
    card on click

--- www/app/modules/template.js
+++ www/app/modules/template.js
@@ -4,6 +4,30 @@ mobileresponseWebbApp
     // =========================================================================
     // MAINMENU COLLAPSE
     // =========================================================================
+    .directive("outsideClick", ['$document', '$parse', function ($document, $parse) {
+        return {
+            link: function ($scope, $element, $attributes) {
+                var scopeExpression = $attributes.outsideClick,
+                    onDocumentClick = function (event) {
+                        var isChild = $element.find(event.target).length > 0;
+
+                        if (!isChild) {
+                            $scope.$apply(scopeExpression);
+                        }
+                    };
+
+                $document.on("click", onDocumentClick);
+
+                $element.on('$destroy', function () {
+                    $document.off("click", onDocumentClick);
+                });
+            }
+        }
+    }])
+
+    // =========================================================================
+    // MAINMENU COLLAPSE
+    // =========================================================================
 
     .directive('toggleSidebar', function(){
         return {
