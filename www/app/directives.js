a2b5fab93b7978edb2de767e107be492913387fd
Robin Pipirs <robinpipirs@gmail.com>
2016-03-30 13:48:04 +0200

    directives.js
    moved directives into directives.js

--- www/app/directives.js
+++ www/app/directives.js
@@ -258,4 +258,23 @@ mobileresponseWebbApp
             }
         }
     })
+
+    // =========================================================================
+    // Focus me
+    // =========================================================================
+
+    .directive('focusMe', function ($timeout) {
+        return {
+            link: function (scope, element, attr) {
+                attr.$observe('focusMe', function (value) {
+                    if (value === "true") {
+                        $timeout(function () {
+                            element[0].focus();
+                        });
+                    }
+                });
+            }
+        };
+    })
+
    
