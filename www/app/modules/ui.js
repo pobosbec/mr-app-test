05ea932e2521a536339580167d973aa362f564f2
Robin Pipirs <robinpipirs@gmail.com>
2016-03-30 13:48:35 +0200

    moved directives
    moved directives

--- www/app/modules/ui.js
+++ www/app/modules/ui.js
@@ -1,21 +1,3 @@
 mobileresponseWebbApp
 
-    // =========================================================================
-    // Focus me
-    // =========================================================================
-    
-
-    .directive('focusMe', function ($timeout) {
-        return {
-            link: function (scope, element, attr) {
-                attr.$observe('focusMe', function (value) {
-                    if (value === "true") {
-                        $timeout(function () {
-                            element[0].focus();
-                        });
-                    }
-                });
-            }
-        };
-    });
 
