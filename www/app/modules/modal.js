de92cafc39e30485b54de03fe88f40d49ea13396
Robin Pipirs <robinpipirs@gmail.com>
2016-03-31 11:35:24 +0200

    typeahead
    typeahead

--- www/app/modules/modal.js
+++ www/app/modules/modal.js
@@ -1,16 +1,24 @@
 // Please note that $uibModalInstance represents a modal window (instance) dependency.
 // It is not the same as the $uibModal service used above.
 angular.module('modalcontroll',[])
-    .controller('createMessageCtrl', function ($scope, $uibModalInstance) {
+    .controller('createMessageCtrl', function ($scope, $uibModalInstance, contactsService) {
 
-    $scope.createMsg = function () {
+        $scope.users = contactsService.getAppUsers();
+        console.log($scope.users);
+        $scope.createMsg = function () {
         alert('noothing');
         $uibModalInstance.close();
-    };
+        };
 
-    $scope.cancel = function () {
+        $scope.find = function (data) {
+            alert(data);
+            console.log(contactsService.getAppUsers());
+        };
+
+        $scope.cancel = function () {
         $uibModalInstance.dismiss('cancel');
-    };
+
+        };
 });/**
  * Created by Robin Jobb on 2016-03-30.
  */
