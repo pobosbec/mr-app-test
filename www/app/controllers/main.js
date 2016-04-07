0cb8293511937afd14ceb4949b90ddfba19b2af1
Robin Pipirs <robinpipirs@gmail.com>
2016-04-07 09:30:08 +0200

    logout
    logout

--- www/app/controllers/main.js
+++ www/app/controllers/main.js
@@ -10,7 +10,7 @@ mobileresponseWebbApp
 
         // process the confirmation dialog result
         function onConfirm(buttonIndex) {
-            if(buttonIndex == 0){
+            if(buttonIndex == 1){
             $rootScope.logout();
             }
         }
