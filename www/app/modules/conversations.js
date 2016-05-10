6f4fd3e41bbde03a722d80e5907877648ac4c5b9
Kristofer Holmgren <kristofer@bosbec.se>
2016-05-10 09:54:27 +0200

    Cosmetic fix.

--- www/app/modules/conversations.js
+++ www/app/modules/conversations.js
@@ -895,27 +895,10 @@ angular.module('conversations', [])
                     }
                 };
 
-                $scope.newMessageEvents = [];
-
                 $scope.conversationParticipants = conversationInfo.Participants;
 
                 $scope.syncMessages = function () {
                     communicationService.syncPeriodMessagesForConversation(conversationInfo.ConversationId, true, $scope.sync.From, $scope.sync.To, 0, 50);
-                }
-
-                $scope.$on('new-messages', function (event, args) {
-                    pushToEvents('Messages recevied from backend.');
-                });
-
-                $scope.$on('messages-added', function (event, args) {
-                    pushToEvents('Messages added to repository.');
-                });
-
-                function pushToEvents(event) {
-                    if ($scope.newMessageEvents.length > 50) {
-                        $scope.newMessageEvents.pop();
-                    }
-                    $scope.newMessageEvents.unshift(event);
                 }
 
                 $scope.close = function () {
