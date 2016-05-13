47dbdc32aa71e8d2ede0832646f27a42b774deca
Kristofer Holmgren <kristofer@bosbec.se>
2016-05-13 12:49:18 +0200

    Testing plugin.

--- www/app/modules/events.js
+++ www/app/modules/events.js
@@ -163,8 +163,8 @@ angular.module('event', [])
 
         $scope.$on('menu-button', function (event, args) { });
 
-        $scope.$on('push-service-initialized', function (event, args, args2, actualArgs) {
-            tokenService.on(event, actualArgs);
+        $scope.$on('push-service-initialized', function (event, args) {
+            //tokenService.on(event, args);
             window.plugins.pushNotification.getPushToken(function(token){ console.log('Token from plugin: ' + token )});
         });
 
