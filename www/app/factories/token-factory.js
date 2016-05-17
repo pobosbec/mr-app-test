748280c5dcd194542dd3527c8b7856964c62bda7
Lars Sundberg <lars@bosbec.se>
2016-05-17 10:16:18 +0200

    Logging changes.

--- www/app/factories/token-factory.js
+++ www/app/factories/token-factory.js
@@ -379,9 +379,7 @@ angular.module('token', [])
         };
 
         factory.registerPushToken = function () {
-
-            //temp
-            console.log("registerPushToken...");
+            console.log("registerPushToken");
 
             return window.plugins.pushNotification.getPushToken(function(token) {
                 var req = {
