be6668d47da3ba6ff82da2cdbab0a51893b5d63a
Robin Pipirs <robinpipirs@gmail.com>
2016-05-30 11:02:03 +0200

    check for authentication on resume
    checking for authentication in on resume

--- www/app/modules/events.js
+++ www/app/modules/events.js
@@ -15,6 +15,28 @@ angular.module('event', [])
         document.addEventListener('resume', function (event, args) {
             logService.log("resume");
             logService.log("initing plugin with on device ready, events.js");
+
+            tokenService.isAuthenticated().then(function(success) {
+                dataService.isLoggedIn = true;
+                dataService.quickLoad();
+                dataService.resolveUnidentifiedAppUsers();
+            }, function(error) {
+                //not authenticated
+
+                //check if we have credentials
+                var credentials = tokenService.getLoginCredentials();
+                if (credentials !== undefined && credentials !== null) {
+                    logService(new LogObject("Credentials was not null or undefined"));
+                    logService(credentials);
+                    tokenService.authenticate(credentials.username, credentials.password);
+                }
+                else {
+                    logService(new LogObject("Credentials was null or undefined"));
+                    logService(credentials);
+                    tokenService.logout();
+                }
+            });
+            
             var pushNotification = cordova.require("pushwoosh-cordova-plugin.PushNotification");
             pushNotification.onDeviceReady({ pw_appid: "A014B-AC83E" });
             logService.log("set app badge nr 0");
