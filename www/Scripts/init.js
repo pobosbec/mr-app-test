acea51faa4974879a8ccdbd42797fad70b154856
BJÖRNCOLLIANDER\Colliander <bjorn@bosbec.se>
2016-11-23 13:42:59 +0100

    re-login device ready

--- www/Scripts/init.js
+++ www/Scripts/init.js
@@ -1,58 +1,18 @@
-﻿//angular.module('deviceReady', [])
-//    .factory('deviceReady',
-//        function() {
+﻿angular.module('deviceReady', [])
+    .factory('deviceReady',
+        function () {
 
-//            function initPushwoosh() {
-
-//                var pushNotification = cordova.require("pushwoosh-cordova-plugin.PushNotification");
-//                pushNotification.onDeviceReady({ pw_appid: "A014B-AC83E" });
-
-//                if (window.isPhoneGap) {
-//                    console.log("initPushwoosh, isPhoneGap");
-//                    if (cordova !== null &&
-//                        typeof cordova !== "undefined" &&
-//                        cordova.require !== null &&
-//                        typeof cordova.require !== "undefined") {
-//                        if (device.platform == "Android") {
-//                            console.log("registering Android");
-//                            registerPushwooshAndroid();
-//                        }
-
-//                        if (device.platform == "iPhone" || device.platform == "iOS") {
-//                            console.log("registering pushwooshIOS");
-//                            registerPushwooshIOS();
-//                        }
-
-//                        if (device.platform == "Win32NT") {
-//                            registerPushwooshWP();
-//                        }
-//                    }
-//                }
-//           }
-
-//            return function (done) {
-//                if (window.isPhoneGap) {
-//                    var deviceInit = function() {
-//                        initPushwoosh();
-//                        done(true);
-//                    };
-
-//                    document.addEventListener('deviceready', deviceInit, false);
-
-//                } else {
-//                    done(false);
-//                }
-//            }
-
-//            //return function(done) {
-//            //    if (typeof window.cordova === 'object') {
-//            //        document.addEventListener('deviceready', initDevice, false);
-//            //    } else {
-//            //        done(false);
-//            //    }
-//            //}
-//        }
-//    );
+            return function (done) {
+                if (typeof window.cordova === 'object') {
+                    document.addEventListener('deviceready', function() {
+                        done(true);
+                    }, false);
+                } else {
+                    done(false);
+                }
+            }
+        }
+    );
 
 var mrApp = angular.module('mrApp', [
     'ngCordova',
@@ -62,7 +22,8 @@ var mrApp = angular.module('mrApp', [
     'mobile-angular-ui',
     'ngSanitize',
     'UsersFactory',
-    'ConversationsFactory'//,'deviceReady'
+    'ConversationsFactory',
+    'deviceReady'
 ]);
 
 mrApp.run(function () {
