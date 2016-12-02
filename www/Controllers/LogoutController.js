262e4c09c682505d7e11af0fc81c5d6d9fefebb5
BJÖRNCOLLIANDER\Colliander <bjorn@bosbec.se>
2016-12-02 10:20:23 +0100

    logout

--- www/Controllers/LogoutController.js
+++ www/Controllers/LogoutController.js
@@ -2,24 +2,36 @@
     '$rootScope', '$localStorage', '$location', '$window', '$timeout', 'DeviceFactory',
     function ($rootScope, $localStorage, $location, $window, $timeout, deviceFactory) {
 
+        function afterLogout(delayTime) {
+            $timeout(function () {
+                $location.path('/login');
+                $window.location.reload();
+            }, delayTime);
+        }
+
         function init() {
             console.log("LOGOUT");
             $rootScope.keepMeSignedIn = false;
             $localStorage.savedCredentials.keepMeSignedIn = $rootScope.keepMeSignedIn;
             $rootScope.authenticationToken = undefined;
 
-            deviceFactory.unregisterDevice(
-                function(status) {
-                    console.log("unregisteredDevice success");
-                },
-                function(status) {
-                    console.log("unregisteredDevice error");
-                });
+            if (deviceFactory.isDevice()) {
+                deviceFactory.unregisterDevice(
+                    function(status) {
+                        alert("unregisteredDevice success");
+                        console.log("unregisteredDevice success");
+                        afterLogout(200);
+                    },
+                    function(status) {
+                        alert("unregisteredDevice error");
+                        console.log("unregisteredDevice error");
+                        afterLogout(200);
+                    });
 
-            $timeout(function () {
-                $location.path('/login');
-                $window.location.reload();
-            }, 2000);
+            } else {
+                afterLogout(1000);
+            }
+            
         }
 
         init();
