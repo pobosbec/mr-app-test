45b009039a000959ce0ed3531ffa6ec2f8626f09
BJÖRNCOLLIANDER\Colliander <bjorn@bosbec.se>
2016-12-14 13:57:31 +0100

    logn test

--- www/Controllers/LoginController.js
+++ www/Controllers/LoginController.js
@@ -216,6 +216,7 @@
                     //console.log($rootScope.currentInboxId);
                     setSigningIn(false);
                     if ($rootScope.currentInboxId != undefined) {
+                        alert("Login complete");
                         $location.path('/conversations/' + $rootScope.currentInboxId);
                     } else {
                         $location.path('/main');
